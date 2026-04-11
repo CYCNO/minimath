f60492a5bde225e339000058203d88105e7cdd0e
CYCNO <daniyalchunna2@gmail.com>
2026-04-11 21:58:26 +0530

    removed useless folder

--- .venv/lib/python3.12/site-packages/minimath/__init__.py
+++
@@ -1,7 +0,0 @@
-from .matrix import Matrix
-from .minimath import MiniMath
-
-__version__ = "0.2"
-__author__ = "CYCNO"
-
-__all__ = ["MiniMath", "Matrix"]
--- .venv/lib/python3.12/site-packages/minimath/minimath.py
+++
@@ -1,7 +0,0 @@
-from .algebra import Algebra
-from .basic import Basic
-from .geometry import Geometry
-
-
-class MiniMath(Basic, Algebra, Geometry):
-    pass
